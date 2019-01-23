38efbc3ec4a5de72f83323a7fa4aed44f7e4ede7
Asutosh Sahoo <asutoshsahoo786@gmail.com>
2019-01-23 22:05:40 +0530

    Removed redundant input format info from main.py

--- main.py
+++ main.py
@@ -1,22 +1,6 @@
 from manage_gcp_instance import manage
 from miscutils import pubsub
 
-'''
-Sample JSON Message for Pub/Sub: 
-{
-    "action": "start|stop|create|delete",
-    "project": "project-name",
-    "zone": "us-central1-a",
-    "instance": "instance-name",
-    "size": "f1-micro",
-    "image-project": "ubuntu-os-cloud",
-    "image_family": "ubuntu-1604-lts",
-    "metadata": {
-       "email" : "someone@somewhere.com
-    }
-}
-'''
-
 def cf_manage_instance(event, context):
     '''
     Function to be executed by the Cloud Function, triggered from a message on a Cloud Pub/Sub topic.
